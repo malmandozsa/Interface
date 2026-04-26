a067121a386634a0d2a2acb126151091a5c977b2
malmandozsa <malmandozsa@alumni.unav.es>
2026-04-26 20:42:11 +0200

    Update ID for historical data sheet

--- update_aulas.py
+++ update_aulas.py
@@ -11,7 +11,7 @@ from google.oauth2.service_account import Credentials
 # ⚙️ CONFIGURACIÓN DE IDS (Sustituye el ID del Historial)
 # ==========================================
 ID_EXCEL_HOY = "1oe6rvKg1zo-Jv7Nd8FJy0FEXolN4yvg7KnaNAAsIs94"
-ID_HISTORIAL = "TU_ID_DEL_EXCEL_HISTORIAL" # <--- CAMBIA ESTO
+ID_HISTORIAL = "1TZz81HseCTH2WHktQgJmoirP1SXbblll" # <--- CAMBIA ESTO
 
 ZONA_HORARIA = "Europe/Madrid"
 IDS_AULAS = [1282, 1283, 1284, 1285, 1286, 1287, 1531, 1535]
